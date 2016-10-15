fc4667046e950861103d3c3f4c0bb1da3d776132
vReda <15vr@queensu.ca>
2016-10-15 13:22:28 -0700

    I think q2 is done?

--- q2.sh
+++ q2.sh
@@ -1,6 +1,15 @@
-Question 2
+#Question 2
 
 #!/bin/bash
 
 clear
-echo 'sup world'
+
+echo "Hello, user. Lets see what your IP is:"
+
+var=$(ifconfig ens33|grep 'inet addr:'|awk '{print $2}'|sed -r 's/.{5}//')
+
+if [[ $var == 127* ]]; then
+	echo "Your IP is $var and this is a Local IP";
+else
+	echo "Your IP is $var and this is a Private IP";
+fi
