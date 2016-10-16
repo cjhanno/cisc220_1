2d7d9ca7aae077bb237b0dc49791dc7eea720805
vReda <15vr@queensu.ca>
2016-10-16 15:37:39 -0700

    first commit w/ syntax problems esfgefds
    vim q4.sh

--- q4.sh
+++ q4.sh
@@ -1,1 +1,21 @@
-Question 4
+#!/bin/bash
+
+
+#permissionSaver.sh
+#clear
+
+n=0
+
+if [ ! -e 'savedPermissions' ] then
+	ls -l | awk '{print $1 " "$NF}' > savedPermissions;
+fi
+
+while [ -e 'savedPermissions'$n ] do
+		$n=$n+1
+done
+ls -l | awk '{print $1 " "$NF}' > savedPermissions$n
+
+
+#end of permissionSaver.sh
+
+
