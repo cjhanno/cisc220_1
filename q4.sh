dff6683ea066ebb806009ce6bb83abb575eab392
vReda <15vr@queensu.ca>
2016-10-17 13:20:04 -0700

    idk anymore

--- q4.sh
+++ q4.sh
@@ -4,17 +4,14 @@
 #permissionSaver.sh
 #clear
 
-i=1
+n=1
 
 if [ ! -e 'savedPermissions' ]; then
 	ls -l | awk '{print $1 " "$NF}' > savedPermissions;
 fi
-
 while [ -e 'savedPermissions$i' ]; do
-	(($i=$i+1))
-	 
+	(($i=$i+1))	 
 done
-n=$i
 ls -l | awk '{print $1 " "$NF}' > savedPermissions$n
 
 #end of permissionSaver.sh
