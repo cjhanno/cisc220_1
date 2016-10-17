91fdc0cb2cf92ebf2415727fe576e95246b569d5
ryan-kartavicius <14rak2@queensu.ca>
2016-10-17 16:14:29 -0500

    First part works now

--- q4.sh
+++ q4.sh
@@ -6,15 +6,15 @@
 
 n=0
 
-if [ ! -e 'savedPermissions' ] then
+if [ ! -e 'savedPermissions' ]; then
 	ls -l | awk '{print $1 " "$NF}' > savedPermissions;
 fi
 
-while [ -e 'savedPermissions'$n ] do
-		$n=$n+1
+while [ -e 'savedPermissions'$n ]; do
+		n=$(($n + 1))
 done
 ls -l | awk '{print $1 " "$NF}' > savedPermissions$n
-
+echo "savedPermissions$n saved"
 
 #end of permissionSaver.sh
 
