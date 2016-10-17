4da44329f3b0a8955b01448aab2f998a0bf81337
vReda <15vr@queensu.ca>
2016-10-17 13:01:24 -0700

    more changes

--- q4.sh
+++ q4.sh
@@ -4,18 +4,18 @@
 #permissionSaver.sh
 #clear
 
-n=1
+i=1
 
 if [ ! -e 'savedPermissions' ]; then
 	ls -l | awk '{print $1 " "$NF}' > savedPermissions;
 fi
 
-while [ -e 'savedPermissions$n' ]; do
-	echo $n++
-	#line above does not work. Point math can't be done but not sure
-	#What kinds of alternatives exist. 
+while [ -e 'savedPermissions$i' ]; do
+	(($i=$i+1))
+	 
 done
-ls -l | awk '{print $1 " "$NF}' > 'savedPermissions$n'
+n=$i
+ls -l | awk '{print $1 " "$NF}' > savedPermissions$n
 
 #end of permissionSaver.sh
 
