583bf1f4b2bc09ad59767520f64cd872a8678c1e
Connor Hannough <12ch33@queensu.ca>
2016-10-17 14:51:38 -0700

    how the hell do you pass whitespace as an index element@!!>!?!?!!?!?

--- q3.sh
+++ q3.sh
@@ -9,11 +9,27 @@ echo "User 1 press 1 to move forward, User 2 press 2 and User 3 press 3"
 #testing track for car 1
 
 carA=("|->")
-carA[40]="# Lane 1 #"
+carA[41]="# Lane 1 #"
 
+#for x in carA
+# if x < 40
+# carA(x) = " "
+
+echo ${carA[*]}
+
+for (( n=4; n<=40; n++ )); do carA[n]="\ "; done
 
 
 echo ${carA[*]}
+
+for (( n=4; n<=40; n++ )); do carA[n]='\ '; done
+
+echo ${carA[*]}
+
+
+#for x in carA ; do carA[x]=" "; done
+#echo ${carA[*]}
+
 
 
 #declare -A carA
