183f61a8fb3226c1b0f80d0d41bc6fd0f0e0a441
Connor Hannough <12ch33@queensu.ca>
2016-10-17 16:30:32 -0700

    getting somewhere (need to figure out whitespace)

--- q3.sh
+++ q3.sh
@@ -1,42 +1,29 @@
 #!/bin/bash
 
-# Question 3
-# start message
 clear
 echo "Welcome to CISC220 Racing Arena"
 echo "User 1 press 1 to move forward, User 2 press 2 and User 3 press 3"
 
-#testing track for car 1
-
 carA=("|->")
+carB=("|->")
+carC=("|->")
+
 carA[41]="# Lane 1 #"
+carB[41]="# Lane 2 #"
+carC[41]="# Lane 3 #"
 
-#for x in carA
-# if x < 40
-# carA(x) = " "
-
-echo ${carA[*]}
-
-for (( n=4; n<=40; n++ )); do carA[n]="\ "; done
+for (( n=4; n<=40; n++ )); do carA[n]="_"; done
+for (( n=4; n<=40; n++ )); do carB[n]="_"; done
+for (( n=4; n<=40; n++ )); do carC[n]="_"; done
 
 
 echo ${carA[*]}
-
-for (( n=4; n<=40; n++ )); do carA[n]='\ '; done
-
-echo ${carA[*]}
+echo ${carB[*]}
+echo ${carC[*]}
 
 
-#for x in carA ; do carA[x]=" "; done
-#echo ${carA[*]}
-
-
-
-#declare -A carA
-#carA=([start]=(|-> [2]=" ")
-
-#echo${carA[start]}
-
+#while loop combined with read var to see what user inputs modify track
+#user enter's 1 then carA append ~ to begining of list
 
 
 
