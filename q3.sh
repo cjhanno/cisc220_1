0314593570055f4f6b24c2d340d86600f3b99675
Connor Hannough <12ch33@queensu.ca>
2016-10-17 14:10:55 -0700

    #track1 probz

--- q3.sh
+++ q3.sh
@@ -2,8 +2,27 @@
 
 # Question 3
 # start message
-echo "Welcome to CISC220 Racing Arena \n"
+clear
+echo "Welcome to CISC220 Racing Arena"
 echo "User 1 press 1 to move forward, User 2 press 2 and User 3 press 3"
+
+#testing track for car 1
+
+carA=("|->")
+carA[40]="# Lane 1 #"
+
+
+
+echo ${carA[*]}
+
+
+#declare -A carA
+#carA=([start]=(|-> [2]=" ")
+
+#echo${carA[start]}
+
+
+
 
 #bonus?
 
