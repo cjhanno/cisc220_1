62d45227e23c40d856c2c67aa06261d9d96e29dc
Connor Hannough <12ch33@queensu.ca>
2016-10-17 13:02:53 -0700

    initial welcome msg adds

--- q3.sh
+++ q3.sh
@@ -1,1 +1,11 @@
-Question 3
+#!/bin/bash
+
+# Question 3
+# start message
+echo "Welcome to CISC220 Racing Arena \n"
+echo "User 1 press 1 to move forward, User 2 press 2 and User 3 press 3"
+
+#bonus?
+
+# read -p "How many users are playing?"
+
