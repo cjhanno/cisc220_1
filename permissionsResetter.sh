b0e557901cb6976a08215e775ad83c7ac6b56dca
vReda <15vr@queensu.ca>
2016-10-17 20:30:44 -0700

    well I tried

--- permissionsResetter.sh
+++ permissionsResetter.sh
@@ -1,14 +1,19 @@
 #!BIN/BASH
 
-# if -s
-#How to take the two parameters?
+echo what would you like to do? 
+echo input -s to show changes or -r to revert changes
+read optionInput
+echo What file would you like to revert to?
+read fileInput
 
+n=1
 
-while true #-s
-	for $file in #third input
+if [ $optionInput == '-s' ]; then
+	for $file in $fileInput
 	do
+		n=$(($n + 1));
 		current=$(stat -c '%A' "$file");
-		old=$(stat -c '%A' "#currentfile"); 
+		old=$(cut -c2 $fileInput[$n]); 
 		if [ "$current" == "$old" ]; then
 			echo "Old: $(stat -c '%A' "$file")";
 			echo "Current: $(stat -c '%A' "#currentfile")";
@@ -16,19 +21,18 @@ while true #-s
 		fi
 	done
 
-done
-#i if -r
-for $file in #third input
-do
-	current=$(stat -c '%A' "$file");
-	old=$(stat -c '%A' "#currentfile");
-	if [ "$current" != "$old" ]; then
-		#change file permissions here	
-	fi
-done
-
-
-
+fi
+if [ $optionInput == '-r' ]; then
+	for $file in $fileInput
+	do
+		n=$(($n + 1));
+		current=$(stat -c '%A' "$file");
+		old=$(cut -c2 $fileInput[$n]);
+		if [ "$current" != "$old" ]; then
+			chmod $old $file	
+		fi
+	done
+fi
 
 echo "$file permissions reset from $current to $old" 
 
