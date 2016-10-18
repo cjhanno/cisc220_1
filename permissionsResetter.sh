690d5d341611321c42402ca9b4d73da80c24ee05
vReda <15vr@queensu.ca>
2016-10-17 18:50:30 -0700

    this is really terrible

--- permissionsResetter.sh
+++ permissionsResetter.sh
@@ -1,21 +1,22 @@
-#!bin/bash
+#!BIN/BASH
 
 # if -s
 #How to take the two parameters?
 
 
+while true #-s
+	for $file in #third input
+	do
+		current=$(stat -c '%A' "$file");
+		old=$(stat -c '%A' "#currentfile"); 
+		if [ "$current" == "$old" ]; then
+			echo "Old: $(stat -c '%A' "$file")";
+			echo "Current: $(stat -c '%A' "#currentfile")";
+			echo "File: $file";
+		fi
+	done
 
-for $file in #third input
-do
-	current=$(stat -c '%A' "$file");
-	old=$(stat -c '%A' "#currentfile"); 
-	if [ "$current" == "$old" ]; then
-		echo "Old: $(stat -c '%A' "$file")";
-		echo "Current: $(stat -c '%A' "#currentfile")";
-		echo "File: $file";
-	fi
 done
-
 #i if -r
 for $file in #third input
 do
