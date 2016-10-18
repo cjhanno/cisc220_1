7ed5a6fe19f1fb7ad32cde7931a146f11a2ae70c
Connor Hannough <12ch33@queensu.ca>
2016-10-17 20:48:41 -0700

    q5 final framing

--- q5.sh
+++ q5.sh
@@ -7,9 +7,21 @@
 
 #ARG 1
 
-Avar=$1
 
 #read -p argument "-s" or "-r" for show and reset
+
+if [ "$1"=="-s" ]; then
+ echo 'show';
+ echo 'file permissions: ' ; #read out permissions
+ cat $2
+
+else 
+echo "reset"; #reset permistions to $Bvar -1
+echo "old file permissions: "
+ #chmod filepermissions$n (where n is $2 - 1)(pipe city here)
+fi
+
+
 
 #if -s do  show files/dir's that have changed permissions
 
@@ -20,5 +32,6 @@ Avar=$1
 
 Bvar=$2
 #name of savedPermissions file created by the permissionsSaver.sh script
+
 
 echo "Do something with $Avar and $Bvar."
