2244b797225859a545b3adce8972fc6ec288571c
Connor Hannough <12ch33@queensu.ca>
2016-10-17 20:00:52 -0700

    q 4 part two pseudocode and framework

--- q5.sh
+++ q5.sh
@@ -1,5 +1,24 @@
+#!/bin/bash
+
 #question 4 part 2
 
 #permission resetter
 
-#ryan doesnt know how to pipe
+
+#ARG 1
+
+Avar=$1
+
+#read -p argument "-s" or "-r" for show and reset
+
+#if -s do  show files/dir's that have changed permissions
+
+#else if -r do reset to old permissions
+
+
+#ARG 2
+
+Bvar=$2
+#name of savedPermissions file created by the permissionsSaver.sh script
+
+echo "Do something with $Avar and $Bvar."
