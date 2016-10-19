190b216e760d3c70328d32bb9162db8002433213
vReda <15vr@queensu.ca>
2016-10-18 20:08:57 -0700

    Last files we need

---
+++ permissionsResetter.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+
+if [ "$1" == "-s" ]; then
+	for line in $2 ; do
+		current=$(ls -l $line* | awk '{print $1 " "$NF}'| cut -c1-10);
+		old=$(stat -c '%A' "$line"); 
+		if [ "$current" == "$old" ]; then
+			echo "Old: $(stat -c '%A' "$line")";
+			echo "Current: $(ls -l $line* | awk '{print $1 " "$NF}' | cut -c1-10)";
+			echo "File: $line";
+		fi
+	done
+
+fi
+if [ "$1" == "-r" ]; then
+	for line in $2; do
+		current=$(ls -l $line* | awk '{print $1 " "$NF}' | cut -c1-10);
+		old=$(stat -c '%A' "$line");
+		if [ "$current" != "$old" ]; then
+			chmod $old $line*;	
+			echo "$line permissions reset from $current to $old";
+		fi
+	done
+fi
+
+
