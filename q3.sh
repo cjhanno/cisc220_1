b1851f67fb9b1ccfee31ac4cd7344884959fcfd5
ryan-kartavicius <14rak2@queensu.ca>
2016-10-18 20:34:53 -0500

    Working #3

--- q3.sh
+++ q3.sh
@@ -1,26 +1,70 @@
 #!/bin/bash
 
-clear
 echo "Welcome to CISC220 Racing Arena"
 echo "User 1 press 1 to move forward, User 2 press 2 and User 3 press 3"
 
-carA=("|->")
-carB=("|->")
-carC=("|->")
+carA[40]='|->'
+carB[40]='|->'
+carC[40]='|->'
+for i in $(seq 41 79); do
+carA[i]='_'
+carB[i]='_'
+carC[i]='_'
+done
 
-carA[41]="# Lane 1 #"
-carB[41]="# Lane 2 #"
-carC[41]="# Lane 3 #"
-
-for (( n=4; n<=40; n++ )); do carA[n]="_"; done
-for (( n=4; n<=40; n++ )); do carB[n]="_"; done
-for (( n=4; n<=40; n++ )); do carC[n]="_"; done
-
+carA[80]='# Lane 1 #'
+carB[80]='# Lane 2 #'
+carC[80]='# Lane 3 #'
 
 echo ${carA[*]}
 echo ${carB[*]}
 echo ${carC[*]}
 
+((counter1=1))
+((counter2=1))
+((counter3=1))
+while true; do
+read input
+
+if [ $input == '1' ]; then
+carA[counter1]='~'
+carA[((counter1 + 40))]=' '
+((counter1=counter1 + 1))
+if [ $counter1 == '41' ]; then
+echo "PLAYER 1 WINS!"
+break
+fi
+
+elif [ $input == '2' ]; then
+carB[counter2]='~'
+carB[((counter2 + 40))]=' '
+((counter2=counter2 + 1))
+if [ $counter2 == '41' ]; then
+echo "PLAYER 2 WINS!"
+break
+fi
+
+elif [ $input == '3' ]; then
+carC[counter3]='~'
+carC[((counter3 + 40))]=' '
+((counter3=counter3 + 1))
+if [ $counter3 == '41' ]; then
+echo "PLAYER 3 WINS!"
+break
+fi
+
+elif [ $input == 'q' ]; then 
+break
+
+else
+echo "Not a valid input"
+
+fi
+clear
+echo ${carA[*]}
+echo ${carB[*]}
+echo ${carC[*]}
+done
 
 #while loop combined with read var to see what user inputs modify track
 #user enter's 1 then carA append ~ to begining of list
